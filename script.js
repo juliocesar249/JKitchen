716cdd0de2d25cf83d4b7b5d04395a85649093ff
juliocesar249 <jc2493718@gmail.com>
2021-11-02 17:30:26 -0300

    files updated

--- script.js
+++ script.js
@@ -16,13 +16,11 @@ buttomContact.onclick = function () {
 }
 
 let address = document.getElementById('contact_address')
-
 address.onclick = function () {
     open('https://www.google.com/maps/search/Algum+lugar/@-20.0306152,-48.9317634,15z')
 }
 
 let email = document.getElementById('contact_email')
-
 email.onclick = function () {
     open('mailto:jkitchen@gmail.com')
 }
@@ -40,4 +38,57 @@ facebook.onclick = function () {
 let instagram = document.getElementById('instagram_icon')
 instagram.onclick = function () {
     open('https://www.instagram.com/algum_perfil.ae/?hl=pt-br')
+}
+
+let checkBoxes = document.querySelectorAll('.menu_item_checkbox')
+
+let itemPriceString = document.querySelectorAll('.price')
+let price = {
+    arrozPrice: Number(itemPriceString[0].outerText.slice(2, -3)),
+    feijaoPrice: Number(itemPriceString[1].outerText.slice(2, -3)),
+    lasanhaPrice: Number(itemPriceString[2].outerText.slice(2, -3)),
+    strogonoffPrice: Number(itemPriceString[3].outerText.slice(2, -3)),
+    costDePorcoPrice: Number(itemPriceString[4].outerText.slice(2, -3)),
+    amondegaPrice: Number(itemPriceString[5].outerText.slice(2, -3))
+}
+
+let imgs = document.querySelectorAll('input.img')
+
+
+let totPrice = 0
+
+function confirmPurchase() {
+    function tot() {
+        if(checkBoxes[0].checked){
+            totPrice += price['arrozPrice']
+        }
+        if(checkBoxes[1].checked) {
+            totPrice += price['feijaoPrice']
+        }
+        if(checkBoxes[2].checked) {
+            totPrice += price['lasanhaPrice']
+        }
+        if(checkBoxes[3].checked) {
+            totPrice += price['strogonoffPrice']
+        }
+        if(checkBoxes[4].checked) {
+            totPrice += price['costDePorcoPrice']
+        }
+        if(checkBoxes[5].checked) {
+            totPrice += price['amondegaPrice']
+        }
+    }
+    tot()
+    
+    let totSum = document.getElementById('totPrice')
+    totSum.innerText = 'R$'+totPrice+',00'
+}
+
+function cancelPurchase() {
+    totPrice = 0
+}
+
+function payPurchase() {
+    totPrice = 0
+    
 }
